8653518d93770a80ca701ac1b3aa1415561e0f6b
Chizzo Cheese <chizzo84@gmail.com>
2017-01-05 19:54:14 +0200

    XPath Example Title Change
    XPath Example PHP File Title Change

--- XPath_Example.php
+++ XPath_Example.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>XPath Example</title>
+    <title>XPath Example using PHP and SimpleXMLElement</title>
 </head>
 
 <body>
